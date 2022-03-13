8ed5ed97f82ba09fe2458bbf099e9624f494ba28
Steven Tegnelia <steventegnelia@Stevens-MacBook-Air.local>
2022-03-13 17:36:35 -0600

    completed frontend on repl, added production key

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,5 +24,10 @@ module.exports = {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
